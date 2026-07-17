47d47c9b10f113ece30bf791da223eafcedc7040
OficialDev <matheus.oficialdev@gmail.com>
2026-07-17 18:53:22 -0300

    update backend/admin/admin.middleware.js

---
+++ backend/admin/admin.middleware.js
@@ -0,0 +1,23 @@
+function somenteAdmin(req,res,next){
+
+
+if(req.usuario.tipo !== "admin"){
+
+
+return res.status(403).json({
+
+erro:"Acesso negado"
+
+});
+
+
+}
+
+
+next();
+
+
+}
+
+
+module.exports=somenteAdmin;
