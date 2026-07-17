90054336cbad2e707f4412485623cfcc71823119
OficialDev <matheus.oficialdev@gmail.com>
2026-07-17 20:53:01 -0300

    update lib/screens/cart/cart_screen.dart

---
+++ lib/screens/cart/cart_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import '../../providers/cart_provider.dart';
+import 'package:provider/provider.dart';
+
+
+class CartScreen extends StatelessWidget {
+
+
+const CartScreen({super.key});
+
+
+@override
+Widget build(BuildContext context){
+
+
+final cart =
+Provider.of<CartProvider>(context);
+
+
+
+return Scaffold(
+
+
+appBar: AppBar(
+
+title:
+const Text("Meu Carrinho"),
+
+),
+
+
+
+body: Column(
+
+children:[
+
+
+Expanded(
+
+child: ListView.builder(
+
+itemCount:
+cart.items.length,
+
+
+itemBuilder:(context,index){
+
+
+final item =
+cart.items[index];
+
+
+
+return ListTile(
+
+
+title:
+Text(item.nome),
+
+
+subtitle:
+Text(
+"Quantidade: ${item.quantidade}"
+),
+
+
+trailing:
+Text(
+"R\$ ${item.subtotal}"
+),
+
+
+);
+
+
+},
+
+),
+
+),
+
+
+
+Text(
+
+"Total: R\$ ${cart.total}",
+
+
+style:
+const TextStyle(
+
+fontSize:22,
+
+fontWeight:
+FontWeight.bold
+
+),
+
+),
+
+
+
+ElevatedButton(
+
+
+onPressed:(){
+
+
+Navigator.pushNamed(
+context,
+"/checkout"
+);
+
+
+},
+
+
+child:
+const Text(
+"Finalizar Pedido"
+),
+
+
+)
+
+
+],
+
+
+),
+
+
+);
+
+
+}
+
+
+}
