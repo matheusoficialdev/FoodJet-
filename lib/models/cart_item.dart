94f4a2c35715f3c4f8460a27916f24f1f5122066
OficialDev <matheus.oficialdev@gmail.com>
2026-07-17 20:16:48 -0300

    update lib/models/cart_item.dart

---
+++ lib/models/cart_item.dart
@@ -0,0 +1,14 @@
+import 'product.dart';
+
+class CartItem {
+  final Product produto;
+  int quantidade;
+
+  CartItem({
+    required this.produto,
+    this.quantidade = 1,
+  });
+
+  double get subtotal =>
+      produto.preco * quantidade;
+}
