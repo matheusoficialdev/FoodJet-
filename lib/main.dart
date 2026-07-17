77bdfb5e94f40377493c0fbfed78c0a4720937ab
OficialDev <matheus.oficialdev@gmail.com>
2026-07-17 19:10:22 -0300

    update lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/splash/splash_screen.dart';
+
+void main() {
+  runApp(const FoodJetApp());
+}
+
+class FoodJetApp extends StatelessWidget {
+  const FoodJetApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FoodJet',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Roboto',
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
