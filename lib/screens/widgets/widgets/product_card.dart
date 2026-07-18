1a7c27571928c512dfb3191a4560d3724f271652
OficialDev <matheus.oficialdev@gmail.com>
2026-07-17 23:54:20 -0300

    update lib/screens/widgets/widgets/product_card.dart

---
+++ lib/screens/widgets/widgets/product_card.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+import '../../../models/product.dart';
+
+
+
+class ProductCard extends StatelessWidget {
+
+
+final Product produto;
+
+
+
+const ProductCard({
+
+super.key,
+
+required this.produto
+
+});
+
+
+
+@override
+Widget build(BuildContext context){
+
+
+
+return Card(
+
+margin:
+
+const EdgeInsets.all(10),
+
+
+
+child:
+
+ListTile(
+
+
+title:
+
+Text(
+
+produto.nome,
+
+style:
+
+const TextStyle(
+
+fontWeight:
+FontWeight.bold
+
+),
+
+),
+
+
+
+subtitle:
+
+Text(
+
+produto.descricao
+
+),
+
+
+
+trailing:
+
+Column(
+
+children:[
+
+
+
+Text(
+
+"R\$ ${produto.preco}"
+
+),
+
+
+
+IconButton(
+
+icon:
+
+const Icon(
+Icons.add_circle,
+
+color:Colors.green
+
+),
+
+
+onPressed:(){
+
+
+},
+
+)
+
+],
+
+
+),
+
+
+
+),
+
+
+);
+
+
+
+}
+
+}
