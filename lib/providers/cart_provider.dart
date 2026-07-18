08c42bad085aca89727f882ef5a347013c4a5da5
OficialDev <matheus.oficialdev@gmail.com>
2026-07-17 23:57:14 -0300

    update lib/providers/cart_provider.dart

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -55,3 +55,127 @@ class CartProvider extends ChangeNotifier {
   }
 
 }
+
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+
+class CartItem {
+
+final Product produto;
+
+int quantidade;
+
+
+CartItem({
+
+required this.produto,
+
+this.quantidade = 1,
+
+});
+
+
+double get subtotal =>
+produto.preco * quantidade;
+
+}
+
+
+
+class CartProvider extends ChangeNotifier {
+
+
+final List<CartItem> _itens=[];
+
+
+List<CartItem> get itens => _itens;
+
+
+
+void adicionar(Product produto){
+
+
+final index = _itens.indexWhere(
+
+(item)=>
+item.produto.id == produto.id
+
+);
+
+
+
+if(index >=0){
+
+_itens[index].quantidade++;
+
+}
+
+else{
+
+_itens.add(
+
+CartItem(
+
+produto:produto
+
+)
+
+);
+
+}
+
+
+notifyListeners();
+
+}
+
+
+
+void remover(Product produto){
+
+
+_itens.removeWhere(
+
+(item)=>
+item.produto.id == produto.id
+
+);
+
+
+notifyListeners();
+
+}
+
+
+
+double get total{
+
+
+double valor=0;
+
+
+for(var item in _itens){
+
+valor += item.subtotal;
+
+}
+
+
+return valor;
+
+}
+
+
+
+void limpar(){
+
+_itens.clear();
+
+notifyListeners();
+
+}
+
+
+
+}
